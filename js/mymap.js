8463d9b6932f34946b95e54f52d20f6b1733018f
wohee <whee0827@naver.com>
2018-11-20 16:46:07 -0600

    project3
    project3

---
+++ js/mymap.js
@@ -0,0 +1,27 @@
+
+function initMap(){
+  var university= {lat: 37.630158, lng: 127.090543};
+  var seongsudong = {lat: 37.548050, lng: 127.047668};
+  var seoulforest = {lat: 37.545292, lng: 127.038521};
+  var map = new google.maps.Map(
+    document.getElementById('map'), {
+    zoom: 12, center: songpa
+    }
+  );
+
+        
+  var marker1 = new google.maps.Marker({
+    position: university,
+    map: map
+  });
+        
+  var marker2 = new google.maps.Marker({
+    position: seongsudong,
+    map: map
+  });
+        
+  var marker3 = new google.maps.Marker({
+    position: seoulforest,
+    map: map
+  });
+}
